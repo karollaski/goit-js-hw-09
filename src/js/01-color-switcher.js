fd13a49f92b7d151b0e159105d433bf71f9815ea
karollaski <karol.laski994@gmail.com>
2023-03-10 18:47:30 +0100

    finished task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let newColor = null;
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+stopBtn.setAttribute('disabled', '');
+
+const bodyChangeColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+};
+
+const startColorSwitcher = element => {
+  element.target.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+
+  newColor = setInterval(bodyChangeColor, 1000);
+};
+
+const stopColorSwitcher = element => {
+  element.target.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
+
+  clearInterval(newColor);
+};
+
+startBtn.addEventListener('click', startColorSwitcher);
+stopBtn.addEventListener('click', stopColorSwitcher);
---
+++ src/js/02-timer.js
@@ -0,0 +1,4 @@
+// Opisany w dokumentacji
+import flatpickr from 'flatpickr';
+// Dodatkowy import stylów
+import 'flatpickr/dist/flatpickr.min.css';
