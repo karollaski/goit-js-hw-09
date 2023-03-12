8a3596a321bf34462ca336ca3d3120226b6658a4
karollaski <karol.laski994@gmail.com>
2023-03-12 12:42:33 +0100

    finished task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+const submitForm = event => {
+  event.preventDefault();
+
+  const { delay, step, amount } = event.currentTarget.elements;
+
+  for (let i = 0; i < amount.value; i++) {
+    let position = i + 1;
+    let delays = Number(delay.value) + step.value * i;
+
+    createPromise(position, delays)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+};
+
+form.addEventListener('submit', submitForm);
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
 }
